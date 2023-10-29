6aa71cfec5dcd8462f5a72ecfc7918c9d0f175a2
ronnie.hu <hulc_it@163.com>
2023-10-29 17:47:08 +0800

    feat: 重写class的toString方法

--- src/com/hlongc/car/BYD.java
+++ src/com/hlongc/car/BYD.java
@@ -4,4 +4,9 @@ public class BYD extends Car {
     public void showBYD() {
         System.out.println("我是比亚迪的show");
     }
+
+    @Override
+    public String toString() {
+        return "BYD{}";
+    }
 }
---
+++ src/com/hlongc/car/Test.java
@@ -0,0 +1,8 @@
+package com.hlongc.car;
+
+public class Test {
+    public static void main(String[] args) {
+        BYD byd = new BYD();
+        System.out.println(byd);
+    }
+}
